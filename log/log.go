9d36f6538a1c8ec181123540b692c52e210c500a
atompi <atompi@foxmail.com>
2024-08-30 17:50:07 +0800

    feat: more log arg

--- log/log.go
+++ log/log.go
@@ -6,8 +6,8 @@ import (
 	"gopkg.in/natefinch/lumberjack.v2"
 )
 
-func InitLogger(logPath string, logLevel string) *zap.Logger {
-	writeSyncer := getLogWriter(logPath)
+func InitLogger(logLevel string, logPath string, maxSize int, maxAge int, comp bool) *zap.Logger {
+	writeSyncer := getLogWriter(logPath, maxSize, maxAge, comp)
 	encoder := getEncoder()
 	level := zapcore.InfoLevel
 	switch logLevel {
@@ -34,13 +34,12 @@ func getEncoder() zapcore.Encoder {
 	return zapcore.NewConsoleEncoder(encoderConfig)
 }
 
-func getLogWriter(logPath string) zapcore.WriteSyncer {
+func getLogWriter(logPath string, maxSize int, maxAge int, comp bool) zapcore.WriteSyncer {
 	lumberJackLogger := &lumberjack.Logger{
-		Filename:   logPath,
-		MaxSize:    100,
-		MaxBackups: 5,
-		MaxAge:     31,
-		Compress:   true,
+		Filename: logPath,
+		MaxSize:  maxSize,
+		MaxAge:   maxAge,
+		Compress: comp,
 	}
 	return zapcore.AddSync(lumberJackLogger)
 }
