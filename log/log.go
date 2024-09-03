30bdf41376b8a4f2c9631ec482de3b6aee52baf1
atompi <atompi@foxmail.com>
2024-09-03 15:39:59 +0800

    fix: output log to given level file only

--- log/log.go
+++ log/log.go
@@ -7,32 +7,32 @@ import (
 )
 
 func InitLogger(logLevel string, logPath string, maxSize int, maxAge int, compress bool) *zap.Logger {
-	debugWriteSyncer := getLogWriter(logPath+".debug.log", maxSize, maxAge, compress)
-	infoWriteSyncer := getLogWriter(logPath+".info.log", maxSize, maxAge, compress)
-	warnWriteSyncer := getLogWriter(logPath+".warn.log", maxSize, maxAge, compress)
-	errorWriteSyncer := getLogWriter(logPath+".error.log", maxSize, maxAge, compress)
+	debugWriteSyncer := newLogWriter(logPath+".debug.log", maxSize, maxAge, compress)
+	infoWriteSyncer := newLogWriter(logPath+".info.log", maxSize, maxAge, compress)
+	warnWriteSyncer := newLogWriter(logPath+".warn.log", maxSize, maxAge, compress)
+	errorWriteSyncer := newLogWriter(logPath+".error.log", maxSize, maxAge, compress)
 
-	encoder := getEncoder()
+	encoder := newEncoder()
 
-	debugCore := zapcore.NewCore(encoder, debugWriteSyncer, zapcore.DebugLevel)
-	infoCore := zapcore.NewCore(encoder, infoWriteSyncer, zapcore.InfoLevel)
-	warnCore := zapcore.NewCore(encoder, warnWriteSyncer, zapcore.WarnLevel)
-	errorCore := zapcore.NewCore(encoder, errorWriteSyncer, zapcore.ErrorLevel)
+	debugCore := zapcore.NewCore(encoder, debugWriteSyncer, newLevelEnablerFunc(zapcore.DebugLevel))
+	infoCore := zapcore.NewCore(encoder, infoWriteSyncer, newLevelEnablerFunc(zapcore.InfoLevel))
+	warnCore := zapcore.NewCore(encoder, warnWriteSyncer, newLevelEnablerFunc(zapcore.WarnLevel))
+	errorCore := zapcore.NewCore(encoder, errorWriteSyncer, newLevelEnablerFunc(zapcore.ErrorLevel))
 
 	tee := zapcore.NewTee(debugCore, infoCore, warnCore, errorCore)
 
-	logger := zap.New(tee)
+	logger := zap.New(tee, zap.AddCaller())
 	return logger
 }
 
-func getEncoder() zapcore.Encoder {
+func newEncoder() zapcore.Encoder {
 	encoderConfig := zap.NewProductionEncoderConfig()
 	encoderConfig.EncodeTime = zapcore.RFC3339TimeEncoder
 	encoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder
 	return zapcore.NewConsoleEncoder(encoderConfig)
 }
 
-func getLogWriter(logPath string, maxSize int, maxAge int, compress bool) zapcore.WriteSyncer {
+func newLogWriter(logPath string, maxSize int, maxAge int, compress bool) zapcore.WriteSyncer {
 	lumberJackLogger := &lumberjack.Logger{
 		Filename: logPath,
 		MaxSize:  maxSize,
@@ -40,4 +40,29 @@ func getLogWriter(logPath string, maxSize int, maxAge int, compress bool) zapcor
 		Compress: compress,
 	}
 	return zapcore.AddSync(lumberJackLogger)
+}
+
+func newLevelEnablerFunc(l zapcore.Level) zap.LevelEnablerFunc {
+	switch l {
+	case zap.DebugLevel:
+		return zap.LevelEnablerFunc(func(level zapcore.Level) bool {
+			return level == zapcore.DebugLevel
+		})
+	case zap.InfoLevel:
+		return zap.LevelEnablerFunc(func(level zapcore.Level) bool {
+			return level == zapcore.InfoLevel
+		})
+	case zap.WarnLevel:
+		return zap.LevelEnablerFunc(func(level zapcore.Level) bool {
+			return level == zapcore.WarnLevel
+		})
+	case zap.ErrorLevel:
+		return zap.LevelEnablerFunc(func(level zapcore.Level) bool {
+			return level == zapcore.ErrorLevel
+		})
+	default:
+		return zap.LevelEnablerFunc(func(level zapcore.Level) bool {
+			return false
+		})
+	}
 }
