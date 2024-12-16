59649e56cc6b929583cb8edaa451cff38ade968e
atompi <atompi@foxmail.com>
2024-12-16 16:06:29 +0800

    feat: add GracefulExit function

--- os/os.go
+++ os/os.go
@@ -3,6 +3,10 @@ package os
 import (
 	"os"
 	"os/exec"
+	"os/signal"
+	"syscall"
+
+	"go.uber.org/zap"
 )
 
 func ExecCmd(command string) (output string, err error) {
@@ -19,4 +23,12 @@ func ExecCmd(command string) (output string, err error) {
 func PathExist(path string) bool {
 	_, err := os.Stat(path)
 	return err == nil || os.IsExist(err)
+}
+
+func GracefulExit() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	s := <-c
+	zap.S().Warnf("a %v signal is received, exiting...", s)
+	os.Exit(0)
 }
